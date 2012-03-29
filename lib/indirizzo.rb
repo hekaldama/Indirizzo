287c8d2a2fe473eaf4b0697015db4d2d3134ee4f
Adam Avilla <adam@avil.la>
2012-03-29 11:07:43 -0700

    Adding convenience method Indirizzo.parse

--- lib/indirizzo.rb
+++ lib/indirizzo.rb
@@ -1,1 +1,17 @@
 require 'indirizzo/address'
+
+module Indirizzo
+
+  ##
+  # === Description
+  # Parses +text+ and returns an Indirizzo::Address object
+  #
+  # === Usage
+  # Indirizzo.parse '123 Main Street, City, ST 12345'
+
+  def self.parse text
+    Address.new text
+  end
+
+end
+
