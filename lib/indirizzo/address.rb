61dbc49b50c2d61ff030ce627e5f6476fed55369
Adam Avilla <adam@avil.la>
2012-03-30 08:03:01 -0700

    Moving the MATCH constant to be in the Address namespace.

--- lib/indirizzo/address.rb
+++ lib/indirizzo/address.rb
@@ -1,20 +1,20 @@
+# The Address class takes a US street address or place name and
+# constructs a list of possible structured parses of the address
+# string.
 module Indirizzo
-  # Defines the matching of parsed address tokens.
-  MATCH = {
-    # FIXME: shouldn't have to anchor :number and :zip at start/end
-    :number   => /^(\d+\W|[a-z]+)?(\d+)([a-z]?)\b/io,
-    :street   => /(?:\b(?:\d+\w*|[a-z'-]+)\s*)+/io,
-    :city     => /(?:\b[a-z][a-z'-]+\s*)+/io,
-    :state    => STATE.regexp,
-    :zip      => /\b(\d{5})(?:-(\d{4}))?\b/o,
-    :at       => /\s(at|@|and|&)\s/io,
-    :po_box => /\b[P|p]*(OST|ost)*\.*\s*[O|o|0]*(ffice|FFICE)*\.*\s*[B|b][O|o|0][X|x]\b/
-  }
-
-  # The Address class takes a US street address or place name and
-  # constructs a list of possible structured parses of the address
-  # string.
   class Address
+    # Defines the matching of parsed address tokens.
+    MATCH = {
+      # FIXME: shouldn't have to anchor :number and :zip at start/end
+      :number   => /^(\d+\W|[a-z]+)?(\d+)([a-z]?)\b/io,
+      :street   => /(?:\b(?:\d+\w*|[a-z'-]+)\s*)+/io,
+      :city     => /(?:\b[a-z][a-z'-]+\s*)+/io,
+      :state    => STATE.regexp,
+      :zip      => /\b(\d{5})(?:-(\d{4}))?\b/o,
+      :at       => /\s(at|@|and|&)\s/io,
+      :po_box => /\b[P|p]*(OST|ost)*\.*\s*[O|o|0]*(ffice|FFICE)*\.*\s*[B|b][O|o|0][X|x]\b/
+    }
+
     attr_accessor :text
     attr_accessor :prenum, :number, :sufnum
     attr_accessor :street
